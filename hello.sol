a382afadf32e4a24b2a907c72e244a5e3ae94dfc
toosolid2003 <toosolid2003@yahoo.fr>
2025-05-03 21:59:21 +0200

    added a deposit function to contract

--- hello.sol
+++ hello.sol
@@ -5,22 +5,35 @@ pragma solidity ^0.8.4;
 
 contract HelloSimple    {
     
-    // mapping adresses et balances
+    // Making sure the contract can receive funds
+    address public owner;
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    receive() external payable {}
+    
+    function getFunds() public view returns(uint)   {
+        return address(this).balance;
+    }
+    // Sending funds from owner to someone else
+
     mapping(address => uint) public balances;
 
     error InsufficientBalance(uint requested, uint available);
     event Sent(uint amount, address sender, address receiver);
 
     function deposit(address to, uint _amount) public   {
-        if (balances[msg.sender] < _amount)
+        if (balances[owner] < _amount)
         revert InsufficientBalance({
             requested: _amount,
-            available: balances[msg.sender]
+            available: balances[owner]
         });
 
-        balances[msg.sender] -= _amount;
+        balances[owner] -= _amount;
         balances[to] += _amount;    
-        emit Sent(_amount, msg.sender, to);
+        emit Sent(_amount, owner, to);
     }
 
 }
